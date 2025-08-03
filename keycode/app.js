e37bdea358d8bdc83ea0ba0c0a84c9acf5ea5314
Vedant Kumar <vedantvisoliya@gmail.com>
2025-08-03 18:41:11 +0530

    project coded

---
+++ keycode/app.js
@@ -0,0 +1,18 @@
+const insertElem = document.getElementById("insert");
+
+window.addEventListener("keydown", (event) => {
+    insertElem.innerHTML = `
+        <div class='key'>
+            ${event.key === " " ? "Space":event.key}
+            <small>event.key</small>
+        </div>
+        <div class='key'>
+            ${event.keyCode}
+            <small>event.keyCode</small>
+        </div>
+        <div class='key'>
+            ${event.code}
+            <small>event.code</small>
+        </div>
+    `;
+});
